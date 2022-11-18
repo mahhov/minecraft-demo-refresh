bd94f87dbdc6263c0a7fb5e43fee176bd55f7c42
mahhov1 <mahhov1@gmail.com>
2022-11-18 08:32:57 -0800

    '2'

--- src/main.js
+++ src/main.js
@@ -9,7 +9,22 @@ fs.readFile(levelPath).then(buffer => {
 		if (error)
 			console.error(error);
 		let oldValue = data.value.Data.value.Time.value;
-		let newValue = [0, 1000];
+		let newValue = [0, 100];
+		data.value.Data.value.Time.value = newValue;
+		let outBuffer = nbt.writeUncompressed(data);
+		await fs.writeFile(levelPath, outBuffer);
+		console.log(`done: ${oldValue} => ${newValue}`);
+	});
+});
+
+levelPath = path.resolve(`${process.env.APPDATA}/.minecraft/saves/Demo_World/level.dat_old`);
+
+fs.readFile(levelPath).then(buffer => {
+	nbt.parse(buffer, async (error, data) => {
+		if (error)
+			console.error(error);
+		let oldValue = data.value.Data.value.Time.value;
+		let newValue = [0, 100];
 		data.value.Data.value.Time.value = newValue;
 		let outBuffer = nbt.writeUncompressed(data);
 		await fs.writeFile(levelPath, outBuffer);
